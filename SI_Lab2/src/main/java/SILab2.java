ebda78e87b67f7a57e8d65ee7f6db76cb4000d48
StankovskaKaterina <52983530+StankovskaKaterina@users.noreply.github.com>
2020-06-13 23:50:17 +0200

    Add files via upload

---
+++ SI_Lab2/src/main/java/SILab2.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SILab2 {
+    public List<String> function(List<String> list) {
+        if (list.size() <= 0) {
+            throw new IllegalArgumentException("List length should be greater than 0");
+        }
+        List<String> numMines = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            if (!list.get(i).equals("#")) { //6
+                int num = 0; //7
+                if (i - 1 >= 0 && list.get(i - 1).equals("#")) { //8
+                    num++;//9
+                }
+                if (i + 1 < list.size() && list.get(i + 1).equals("#")) { //10
+                    num++; //11
+                }
+                numMines.add(String.valueOf(num)); //12
+            } else { //13
+                numMines.add(list.get(i)); //14
+            }
+        }
+        return numMines; //15
+    }
+}
---
+++ SI_Lab2/src/test/java/SILab2Test.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SILab2Test {
+@Test public void every_statement_test{
+
+
+    }
+}
